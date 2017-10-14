c2456941e38320a655198faca8ec3f6af667da3c
jsongo <jsongo@qq.com>
2017-10-14 14:37:51 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# 
+# Angular的animation动画演示：进入和退出  
 
 > 该示例中的数据来自于：https://studywebdevelopment.com/top-programming-languages-to-learn-in-2017.html
--- src/app/language/mock.data.ts
+++ src/app/language/mock.data.ts
@@ -4,4 +4,40 @@ export const mockData: Language[] = [{
     name: 'JavaScript',
     intro: 'JavaScript is the most commonly used programming language in the world. ES2017 (the updated JavaScript) is launching later this year which will make many developers emotional (with joy).',
     logo: 'javascript-logo.jpg',
+}, {
+    name: 'Java',
+    intro: 'Java is always a good long term bet and looking at the stats, it doesn’t seem to be going away any time soon. It’s used by over 10 million developers and 15 billion (that’s not a typo) devices run Java worldwide!',
+    logo: 'java-logo.jpg',
+}, {
+    name: 'Python',
+    intro: 'Python is similar to PHP and Ruby in the sense that it is an object-orientated language. It closely resembles the English language so it’s a great language to learn for beginners as well as seasoned professionals.',
+    logo: 'python-logo.jpg',
+}, {
+    name: 'PHP',
+    intro: 'PHP is the most popular server-side programming language. It\'s often used as the foundation of CMS’ (Content Management Systems) like WordPress and big websites like Facebook and Wikipedia.',
+    logo: 'php-logo.jpg',
+}, {
+    name: 'Ruby on Rails',
+    intro: 'Ruby on Rails (a noteworthy framework) is like ‘jQuery for JavaScript’. It makes it much easier to use Ruby, but it’s advised that you have a good understanding of Ruby before you utilize Rails.',
+    logo: 'ruby-on-rails-logo.jpg',
+}, {
+    name: 'Rust',
+    intro: 'Rust is a general-purpose language that helps developers create fast, secure applications which takes advantage of the powerful features of modern multi-core processors.',
+    logo: 'rust-logo.jpg',
+}, {
+    name: 'Elixir',
+    intro: 'Elixir is a dynamic, functional language designed for building scalable and maintainable applications. One of the main benefits is concurrency. In short, it’s great for large applications that handle a lot of tasks at once.',
+    logo: 'elixir-logo.jpg',
+}, {
+    name: 'GOLANG',
+    intro: 'Go (or GOLANG) is created by Google and it\'s gaining popularity which will only grow in 2017. It compiles fast and it has an excellent standard library that is also great with concurrent programs as well.',
+    logo: 'golang-logo.jpg',
+}, {
+    name: 'C#',
+    intro: 'C# (‘see-sharp’) is not only limited to the programming language for Microsoft’s .NET Framework. It’s also used for Windows applications and Android/iOS Apps with the technology from Xamarin.',
+    logo: 'c-sharp-logo.jpg',
+}, {
+    name: 'Swift',
+    intro: 'Swift is one of the fastest growing programming languages in history! It’s built by Apple (not the granny smith apple) and they have big plans for it so it would be good to take note of it as the popularity grows.',
+    logo: 'swift-logo.jpg',
 }];
