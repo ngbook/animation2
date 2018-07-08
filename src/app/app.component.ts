7d0e6df05534657af3773004d13a5df0c830e217
jsongo <jsongo@qq.com>
2018-07-08 16:11:01 +0800

    format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,40 @@ import { Language } from './language/language.model';
             })),
             transition(':enter', [ // void => *
                 animate('500ms ease-in', keyframes([
-                    style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
-                    style({opacity: 1, transform: 'translateX(30px)',  offset: 0.3}),
-                    style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+                    style({
+                        opacity: 0,
+                        transform: 'translateX(-100%)',
+                        offset: 0
+                    }),
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(30px)',
+                        offset: 0.3
+                    }),
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(0)',
+                        offset: 1.0
+                    })
                 ]))
             ]),
             transition(':leave',  // * => void
                 animate('500ms ease-out', keyframes([
-                    style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
-                    style({opacity: 1, transform: 'translateX(-30px)', offset: 0.7}),
-                    style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(0)',
+                        offset: 0
+                    }),
+                    style({
+                        opacity: 1,
+                        transform: 'translateX(-30px)',
+                        offset: 0.7
+                    }),
+                    style({
+                        opacity: 0,
+                        transform: 'translateX(100%)',
+                        offset: 1.0
+                    })
                 ]))
             ),
         ]),
